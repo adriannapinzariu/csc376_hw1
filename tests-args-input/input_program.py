8481edfd99ecce30ec03da63ff1d419fb799689d
adriannapinzariu <121136061+adriannapinzariu@users.noreply.github.com>
2024-10-16 14:06:22 -0500

    finished

--- tests-args-input/input_program.py
+++ tests-args-input/input_program.py
@@ -1,24 +1,36 @@
 #! python
-
 import sys
 
+# READ FROM STANDARD INPUT
 print("Standard Input: ")
-argc= len(sys.argv)
-#print('There are ' + str(argc) + 'command line arguments')
-#sys.argv is a list of CL args stored as strings
-for arg in sys.argv:   
-    print(arg)
-    if arg == '-o': # ALWAYS followed by word
-        if ()
-    elif arg == '-t': # ALWAYS followed by word
-        pass
-    elif arg == '-h': # flag, no text
-        pass
-
+#print each line IMMEDIATELY
 text = sys.stdin.readline()
-lines = []
 while text:
-    lines.append(text)
+    print(text, end='')
     text = sys.stdin.readline()
-for line in lines:
-    print(line, end='')
+
+# HANDLE ARGS
+op1 = ""
+op2 = ""
+f1, f2, f3 = False, False, False # flags
+
+for i in range(len(sys.argv)):   
+    arg = sys.argv[i]
+    if arg == '-o': # ALWAYS followed by word
+        f1 = True
+        op1 = sys.argv[i+1] # store next
+    elif arg == '-t': # ALWAYS followed by word
+        f2 = True
+        op2 = sys.argv[i+1] # store next
+    elif arg == '-h': # flag, no text
+       f3 = True
+
+print("Command line arguments: ")
+if f1:
+    print('option 1: ' + op1)
+if f2:
+    print('option 2: ' + op2)
+if f3:
+    print('option 3')
+
+#check if i need to handle cases where they're present without values
